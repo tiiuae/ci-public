2a0033b075d91c0ed2879ff31593ae9364a03a1a
Tero Tervala <tero.tervala@unikie.com>
2022-10-19 22:26:46 +0300

    Refactor and add comments to hydrascrape
    Signed-off-by: Tero Tervala <tero.tervala@unikie.com>

--- hydrascrape/hydrascrape.py
+++ hydrascrape/hydrascrape.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env pipenv-shebang
+# ------------------------------------------------------------------------
 # SPDX-License-Identifier: MIT
 # SPDX-FileCopyrightText: 2022 Tero Tervala <tero.tervala@unikie.com>
 # SPDX-FileCopyrightText: 2022 Unikie
+# ------------------------------------------------------------------------
+# Script for extracting build information from a hydra server to
+# facilitate further processing in e.g. Jenkins environment.
+# ------------------------------------------------------------------------
 
 import bs4
 import sys
@@ -14,22 +19,31 @@ import filelock
 import json
 from urllib.error import HTTPError
 
-debug = False
-jsonen = False
-hid_proj = False
-dis_proj = False
-hid_jobset = False
-dis_jobset = False
+# ------------------------------------------------------------------------
+# Global debug flag
+# ------------------------------------------------------------------------
+debug = 0
 
 
+# ------------------------------------------------------------------------
+# Convert string to an int with default value if invalid string given
+# str = String to convert
+# default = Default value which is zero by default
+# Returns an integer, always
+# ------------------------------------------------------------------------
 def convert_int(str, default = 0):
+    # Try converting to integer
     try:
         i = int(str)
+    # If it fails, use the default value
     except (ValueError, TypeError):
         i = default
     return i
 
 
+# ------------------------------------------------------------------------
+# Print help and exit
+# ------------------------------------------------------------------------
 def help():
     print("")
     print("Usage: python3 hydrascrape.py <server> <project regexp> <jobset regexp> <handled builds file> <action> [options]")
@@ -52,18 +66,25 @@ def help():
     sys.exit(0)
 
 
-def get_page(urlctx, url):
+# ------------------------------------------------------------------------
+# Fetches a page from given web site
+# context = Connection context
+# url = URL of the page
+# Returns page data, on error returns empty page
+# ------------------------------------------------------------------------
+def get_page(context, url):
 
     if debug:
         print(f"Fetching: {url}")
 
-    req = urllib.request.Request(url, headers=urlctx["headers"])
+    req = urllib.request.Request(url, headers = context['headers'])
     try:
         response = urllib.request.urlopen(req)
     except HTTPError as error:
         print(f"HTTP error: {error}", file = sys.stderr)
         return ""
 
+    # Decompress if gzipped content
     if response.info().get('Content-Encoding') == 'gzip':
         text = gzip.decompress(response.read())
     elif response.info().get('Content-Encoding') == 'deflate':
@@ -77,9 +98,13 @@ def get_page(urlctx, url):
     return text
 
 
-def get_builds(urlctx, eval):
-    hydra = urlctx['hydraurl']
-    text = get_page(urlctx, f"{hydra}eval/{eval}")
+# ------------------------------------------------------------------------
+# Fetches builds for given evaluation
+# context = Connection context
+# Returns a list of build numbers
+# ------------------------------------------------------------------------
+def get_builds(context, eval):
+    text = get_page(context, f"{context['hydra_url']}eval/{eval}")
     soup = bs4.BeautifulSoup(text, features="html.parser")
 
     builds = []
@@ -96,6 +121,11 @@ def get_builds(urlctx, eval):
     return builds
 
 
+# ------------------------------------------------------------------------
+# Reads handled builds from handled builds file
+# filename = Name of the handled builds file
+# Returns a list of build numbers
+# ------------------------------------------------------------------------
 def get_handled(filename):
     handled = []
     try:
@@ -127,6 +157,11 @@ def get_handled(filename):
     return handled
 
 
+# ------------------------------------------------------------------------
+# Writes handled builds into handled builds file
+# filename = name of the handled builds file
+# handled = list of handled build numbers
+# ------------------------------------------------------------------------
 def update_handled(filename, handled):
     handled.sort()
     try:
@@ -139,25 +174,32 @@ def update_handled(filename, handled):
         print(f"Cannot update {filename}: {pe.strerror}", file = sys.stderr)
         sys.exit(1)
 
-
-def get_build_info(urlctx, bnum):
-    summarykeys = {
-        "Build ID": 0,
-        "Status": 1,
-        "System": 0,
-        "Nix name": 0,
-        "Finished at": 0,
-        "Nr": -1,
-    }
-    detailkeys = [
-        "Queued at",
-        "Build started",
-        "Build finished",
-        "Derivation store path",
-        "Output store paths",
-    ]
-    hydra = urlctx['hydraurl']
-    text = get_page(urlctx, f"{hydra}build/{bnum}")
+# ------------------------------------------------------------------------
+# Constant data used by following get_build_info function
+# ------------------------------------------------------------------------
+summarykeys = {
+    "Build ID": 0,
+    "Status": 1,
+    "System": 0,
+    "Nix name": 0,
+    "Finished at": 0,
+    "Nr": -1,
+}
+detailkeys = [
+    "Queued at",
+    "Build started",
+    "Build finished",
+    "Derivation store path",
+    "Output store paths",
+]
+# ------------------------------------------------------------------------
+# Fetches information for given build number
+# Returns a dictionary with information of the build
+# context = Connection context
+# bnum = Build ID
+# ------------------------------------------------------------------------
+def get_build_info(context, bnum):
+    text = get_page(context, f"{context['hydra_url']}build/{bnum}")
     soup = bs4.BeautifulSoup(text, features="html.parser")
 
     binfo = {}
@@ -218,15 +260,22 @@ def get_build_info(urlctx, bnum):
     return binfo
 
 
+# ------------------------------------------------------------------------
+# Sets environment variables based on given dictionary
+# binfo = Dictionary containing build information
+# Returns environment dictionary
+# ------------------------------------------------------------------------
 def set_env(binfo):
     env = os.environ.copy()
 
     for key in binfo:
         if key == "Output store paths":
+            # Set the plain hash of the first output separately
             env["HYDRA_OUTPUT_STORE_HASH"] = binfo[key][0].removeprefix("/nix/store/").split('-', 1)[0]
             env["HYDRA_OUTPUT_STORE_PATHS"] = ','.join(binfo[key])
             continue
         if key == "Derivation store path":
+            # Set the plain hash of the derivation separately
             env["HYDRA_DERIVATION_STORE_HASH"] = binfo[key].removeprefix("/nix/store/").split('-', 1)[0]
         if key == "Inputs":
             env["HYDRA_INPUTS"] = ""
@@ -241,24 +290,32 @@ def set_env(binfo):
     return env
 
 
+# ------------------------------------------------------------------------
+# Saves given given build information dictionary in json format
+# binfo = Dictionary containing build information
+# ------------------------------------------------------------------------
 def save_json(binfo):
-    if jsonen:
-        filename = f"{binfo['Build ID']}.json"
-        if debug:
-            print(f"Writing json info into {filename}")
+    filename = f"{binfo['Build ID']}.json"
+    if debug:
+        print(f"Writing json info into {filename}")
 
-        json_obj = json.dumps(binfo, indent=2)
+    json_obj = json.dumps(binfo, indent=2)
 
-        try:
-            with open(filename, "w") as outf:
-                outf.write(json_obj)
-        except PermissionError as pe:
-            print(f"Could not write {pe.filename}: {pe.strerror}", file = sys.stderr)
-            sys.exit(1)
+    try:
+        with open(filename, "w") as outf:
+            outf.write(json_obj)
+
+    except PermissionError as pe:
+        print(f"Could not write {pe.filename}: {pe.strerror}", file = sys.stderr)
+        sys.exit(1)
 
 
-def get_projects(urlctx, hidden=False, disabled=False):
-    text = get_page(urlctx, urlctx['hydraurl'])
+# ------------------------------------------------------------------------
+# Gets projects from a hydra site
+# context = Connection context
+# ------------------------------------------------------------------------
+def get_projects(context):
+    text = get_page(context, context['hydra_url'])
     soup = bs4.BeautifulSoup(text, features="html.parser")
 
     projects = soup.find_all("tr", {"class": "project"})
@@ -277,12 +334,12 @@ def get_projects(urlctx, hidden=False, disabled=False):
     for d in disableds:
         dlist.append(d.find("a", {"class": "row-link"})["href"].split("/")[-1])
 
-    if hidden == False:
+    if context['hid_proj'] == False:
         for h in hlist:
             if h in plist:
                 plist.remove(h)
 
-    if disabled == False:
+    if context['dis_proj'] == False:
         for d in dlist:
             if d in plist:
                 plist.remove(d)
@@ -293,9 +350,12 @@ def get_projects(urlctx, hidden=False, disabled=False):
     return plist
 
 
-def get_jobsets(urlctx, project, hidden=False, disabled=False):
-    hydra = urlctx['hydraurl']
-    text = get_page(urlctx, f"{hydra}project/{project}")
+# ------------------------------------------------------------------------
+# Gets jobsets for a given hydra project
+# context = Connection context
+# ------------------------------------------------------------------------
+def get_jobsets(context, project):
+    text = get_page(context, f"{context['hydra_url']}project/{project}")
     soup = bs4.BeautifulSoup(text, features="html.parser")
 
     jobsets = soup.find_all("tr", {"class": "jobset"})
@@ -314,12 +374,12 @@ def get_jobsets(urlctx, project, hidden=False, disabled=False):
     for d in disableds:
         dlist.append(d.find("a", {"class": "row-link"})["href"].split("/")[-1])
 
-    if hidden == False:
+    if context['hid_jobset'] == False:
         for h in hlist:
             if h in jlist:
                 jlist.remove(h)
 
-    if disabled == False:
+    if context['dis_jobset'] == False:
         for d in dlist:
             if d in jlist:
                 jlist.remove(d)
@@ -329,13 +389,17 @@ def get_jobsets(urlctx, project, hidden=False, disabled=False):
 
     return jlist
 
-
+# ------------------------------------------------------------------------
+# Handles a given jobset of given project
+# context = Connection context
+# project = Project name
+# jobset = Jobset name
+# handled = list of handled builds
+# ------------------------------------------------------------------------
 def handle_jobset(context, project, jobset, handled):
-    server = context['server']
-    hydra = context['hydraurl']
     # It is assumed here that all evaluations containing builds that need processing
     # are found on the first page of evaluations listing
-    text = get_page(context, f"{hydra}jobset/{project}/{jobset}")
+    text = get_page(context, f"{context['hydra_url']}jobset/{project}/{jobset}")
 
     soup = bs4.BeautifulSoup(text, features="html.parser")
 
@@ -347,7 +411,7 @@ def handle_jobset(context, project, jobset, handled):
     # Remove duplicates
     builds = list(dict.fromkeys(builds))
 
-    newbs = []
+    new_handled = []
 
     for i in builds:
         if i not in handled:
@@ -367,17 +431,21 @@ def handle_jobset(context, project, jobset, handled):
                 continue
 
             if status == "Success":
-                binfo['Server'] = server
+                binfo['Server'] = context['server']
                 binfo['Project'] = project
                 binfo['Jobset'] = jobset
                 del binfo['Status']
                 env = set_env(binfo)
-                save_json(binfo)
+                if context['json_en']:
+                    save_json(binfo)
                 if debug:
                     print(f"Handling {i} " + "-" * 60)
+
+                # Run the user specified action with build info in environment
                 sp = subprocess.run(context['action'], shell=True, env=env)
+
                 if sp.returncode == 0:
-                    newbs.append(i)
+                    new_handled.append(i)
                     if debug:
                         print("Handling successful " + "-" * 55)
                 else:
@@ -386,50 +454,37 @@ def handle_jobset(context, project, jobset, handled):
             else:
                 if debug:
                     print(f"Build {i} has failed, just marking as handled")
-                newbs.append(i)
+                new_handled.append(i)
         else:
             if debug:
                 print(f"Build {i} already handled")
 
-    return newbs
+    return new_handled
 
 
-def main_locked(argv):
-    server = argv[0]
-    filename = argv[3]
-    context = {
-    "server": server,
-    "hydraurl": f"https://{server}/",
-    "headers": {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-                'Accept-Encoding': 'gzip, deflate',
-                'User-Agent': 'hydrascraper.py v1.0'},
-    "action": argv[4],
-    }
+# ------------------------------------------------------------------------
+# locked main program, called only if lock was aqcuired successfully
+# context = Connection context
+# ------------------------------------------------------------------------
+def main_locked(context):
+    context['hydra_url'] = f"https://{context['server']}/"
+    context['headers'] = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                          'Accept-Encoding': 'gzip, deflate',
+                          'User-Agent': 'hydrascraper.py v1.0'}
 
-    r = []
-    for i in [0,1]:
-        try:
-            r.append(re.compile(argv[i + 1]))
-        except re.error as e:
-            if e.pos != None:
-                print(argv[i + 1], file = sys.stderr)
-                print(" " * e.pos + "^", file = sys.stderr)
-            print(f"Regular expression error: {e.msg}", file = sys.stderr)
-            sys.exit(1)
+    handled = get_handled(context['handled_file'])
 
-    handled = get_handled(filename)
-
-    projects = get_projects(context, hidden=hid_proj, disabled=dis_proj)
-    projects = list(filter(r[0].match, projects))
+    projects = get_projects(context)
+    projects = list(filter(context['re_p'].match, projects))
     if debug:
         print("Selected projects: ", projects)
 
     jobsets = {}
     for p in projects:
-        jobsets[p] = get_jobsets(context, p, hidden=hid_jobset, disabled=dis_jobset)
+        jobsets[p] = get_jobsets(context, p)
 
     for p in projects:
-        jobsets[p] = list(filter(r[1].match, jobsets[p]))
+        jobsets[p] = list(filter(context['re_js'].match, jobsets[p]))
         if debug:
             print(f"{p} selected jobsets: {jobsets[p]}")
 
@@ -439,54 +494,77 @@ def main_locked(argv):
             jobset = j
             handled += handle_jobset(context, project, jobset, handled)
 
-    update_handled(filename, handled)
+    update_handled(context['handled_file'], handled)
 
 
-def json_e():
-    global jsonen
-    jsonen = True
+# ------------------------------------------------------------------------
+# Enable JSON output in context
+# context = Connection context
+# ------------------------------------------------------------------------
+def json_e(context):
+    context['json_en'] = True
     if debug:
         print("JSON enabled")
 
 
-def debug_e():
+# ------------------------------------------------------------------------
+# Set global debug flag
+# ------------------------------------------------------------------------
+def debug_e(_):
     global debug
     debug = 1
     print("Debug enabled")
 
 
-def hp_e():
-    global hid_proj
-    hid_proj = True
+# ------------------------------------------------------------------------
+# Set hidden projects flag in context
+# context = Connection context
+# ------------------------------------------------------------------------
+def hp_e(context):
+    context['hid_proj'] = True
     if debug:
         print("Including hidden projects")
 
 
-def dp_e():
-    global dis_proj
-    dis_proj = True
+# ------------------------------------------------------------------------
+# Set disabled projects flag in context
+# context = Connection context
+# ------------------------------------------------------------------------
+def dp_e(context):
+    context['dis_proj'] = True
     if debug:
         print("Including disabled projects")
 
 
-def hj_e():
-    global hid_jobset
-    hid_jobset = True
+# ------------------------------------------------------------------------
+# Set hidden jobsets flag in context
+# context = Connection context
+# ------------------------------------------------------------------------
+def hj_e(context):
+    context['hid_jobset'] = True
     if debug:
         print("Including hidden jobsets")
 
 
-def dj_e():
-    global dis_jobset
-    dis_jobset = True
+# ------------------------------------------------------------------------
+# Set disabled jobsets flag in context
+# context = Connection context
+# ------------------------------------------------------------------------
+def dj_e(context):
+    context['dis_jobset'] = True
     if debug:
         print("Including disabled jobsets")
 
-
+# ------------------------------------------------------------------------
+# Main function
+# argv = Command line parameters
+# ------------------------------------------------------------------------
 def main(argv):
     global debug
+    # Set debug if set in environment
     debug = convert_int(os.getenv("DEBUG"))
 
+    # Map options to functions setting the flags
     argfu = {"-json": json_e,
              "-debug": debug_e,
              "-hp": hp_e,
@@ -495,14 +573,42 @@ def main(argv):
              "-dj": dj_e,
     }
 
+    # Default settings in context
+    context = {'json_en': False,
+               'dis_proj': False,
+               'hid_proj': False,
+               'dis_jobset': False,
+               'hid_jobset': False}
+
+    # Help user, too few arguments given
     if len(argv) < 5:
         help()
 
+    context['server'] = argv[0]
+
+    r = []
+    for i in [0,1]:
+        try:
+            # Create regular expression objects of project and jobset strings
+            r.append(re.compile(argv[i + 1]))
+        except re.error as e:
+            if e.pos != None:
+                print(argv[i + 1], file = sys.stderr)
+                print(" " * e.pos + "^", file = sys.stderr)
+            print(f"Regular expression error: {e.msg}", file = sys.stderr)
+            sys.exit(1)
+
+    context['re_p'] = r[0]
+    context['re_js'] = r[1]
+    context['handled_file'] = argv[3]
+    context['action'] = argv[4]
+
     if len(argv) > 5:
+        # Process options
         for i in range(5, len(argv)):
             f = argfu.get(argv[i], None)
             if f != None:
-                f()
+                f(context)
             else:
                 print(f"Invalid argument: {argv[i]}", file=sys.stderr)
                 sys.exit(1)
@@ -510,7 +616,7 @@ def main(argv):
     lock = filelock.FileLock(f"{argv[3]}.lock")
     try:
         lock.acquire(timeout=0)
-        main_locked(argv)
+        main_locked(context)
 
     except filelock.Timeout as to:
         if debug:
@@ -524,5 +630,8 @@ def main(argv):
         lock.release()
 
 
+# ------------------------------------------------------------------------
+# Run main when executed from command line
+# ------------------------------------------------------------------------
 if __name__ == "__main__":
     main(sys.argv[1:])
