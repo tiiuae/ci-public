38524cb97355a5bc915ab031a7a75765043aa034
Marko Lindqvist <marko.lindqvist@unikie.com>
2023-05-10 10:20:41 +0300

    jenkins-agent: Add initial container creation
    Signed-off-by: Marko Lindqvist <marko.lindqvist@unikie.com>

---
+++ containers/jenkins-agent/run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# SPDX-FileCopyrightText: 2022-2023 Technology Innovation Institute (TII)
+# SPDX-License-Identifier: Apache-2.0
+
+# Normal container run - not the first run that does setup work instead.
+
+/root/.nix-profile/bin/sshd &
+
+/bin/sh
