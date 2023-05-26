8ad830dfd6cbfdf110897f9048a9af77d0f0c837
Marko Lindqvist <marko.lindqvist@unikie.com>
2023-05-26 18:33:53 +0300

    hydra: Set channel "22.11" explicitly
    Do not assume that it's the nix default
    
    Signed-off-by: Marko Lindqvist <marko.lindqvist@unikie.com>

--- containers/hydra/channel.sh
+++ containers/hydra/channel.sh
@@ -9,6 +9,8 @@ if [ "$1" != "" ] ; then
   # We have to get hydra from nixos-22.05, to avoid
   # https://github.com/NixOS/nix/issues/6981
   nix-channel --add https://nixos.org/channels/nixos-${1}-small nixos-${1}
+else
+  nix-channel --add https://nixos.org/channels/nixos-22.11-small nixos-22.11
 fi
 
 nix-channel --update
