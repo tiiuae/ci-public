6e497b319a5262bca8fc368d4f69701c24bf85b2
Marko Lindqvist <marko.lindqvist@unikie.com>
2023-05-26 10:14:43 +0300

    hydra: Fix handling of HC_CHANNEL setting
    Signed-off-by: Marko Lindqvist <marko.lindqvist@unikie.com>

--- containers/hydra/channel.sh
+++ containers/hydra/channel.sh
@@ -6,8 +6,6 @@
 # This script runs during the container setup.
 
 if [ "$1" != "" ] ; then
-  # We have to get hydra from nixos-22.05, to avoid
-  # https://github.com/NixOS/nix/issues/6981
   nix-channel --add https://nixos.org/channels/nixos-${1}-small nixos-${1}
 fi
 
