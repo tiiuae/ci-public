d201446dbb54a3114428ded2b54c75917a42fc1f
Marko Lindqvist <marko.lindqvist@unikie.com>
2023-07-06 10:57:15 +0300

    hydra: Fix upload.sh
    Signed-off-by: Marko Lindqvist <marko.lindqvist@unikie.com>

--- containers/hydra/upload.sh
+++ containers/hydra/upload.sh
@@ -6,15 +6,17 @@
 
 # Callback script called after a package has been built
 
-if [ -f /home/hydra/confs/binarycache.conf ] && [ -f /home/hydra/.ssh/bcache.key ] ; then
+if [ -f /home/hydra/confs/binarycache.conf ] ; then
   . /home/hydra/confs/binarycache.conf
-  if [ -n "$CACHE_SERVER" ] ; then
-    if [ -n "$CACHE_PORT" ] ; then
-      export NIX_SSHOPTS="-i /home/hydra/.ssh/bcache.key -p $CACHE_PORT"
-    else
-      export NIX_SSHOPTS="-i /home/hydra/.ssh/bcache.key"
-    fi
+  if [ -f "$CACHE_SSH_KEY_FILE" ] ; then
+    if [ -n "$CACHE_SERVER" ] ; then
+      if [ -n "$CACHE_PORT" ] ; then
+        export NIX_SSHOPTS="-i $CACHE_SSH_KEY_FILE -p $CACHE_PORT"
+      else
+        export NIX_SSHOPTS="-i $CACHE_SSH_KEY_FILE"
+      fi
 
-    nix-copy-closure --to hydra@$CACHE_SERVER $OUT_PATHS $DRV_PATH
+      nix-copy-closure --to $CACHE_SERVER $OUT_PATHS $DRV_PATH
+    fi
   fi
 fi
