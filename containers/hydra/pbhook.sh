48360f70b2cbf0b7a0c89d3a4a2750875fd86059
Marko Lindqvist <marko.lindqvist@unikie.com>
2023-07-18 09:00:45 +0300

    hydra: Support package signing
    Just create a signature file, to see that signing functionality
    is there. This exact form is unlikely to be used as the final
    way to transfer the singature.
    
    Signed-off-by: Marko Lindqvist <marko.lindqvist@unikie.com>

---
+++ containers/hydra/pbhook.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# SPDX-FileCopyrightText: 2022-2023 Technology Innovation Institute (TII)
+# SPDX-License-Identifier: Apache-2.0
+
+# Callback script called after a package has been built
+
+/home/hydra/scripts/sign.sh
+/home/hydra/scripts/upload.sh
