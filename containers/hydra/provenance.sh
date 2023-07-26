c137741f2456e65cadfa590c4d6b724f860ba471
Joonas Rautiola <joonas.rautiola@unikie.com>
2023-07-26 12:55:32 +0300

    Sign provenance file and upload both to cache
    Signed-off-by: Joonas Rautiola <joonas.rautiola@unikie.com>

--- containers/hydra/provenance.sh
+++ containers/hydra/provenance.sh
@@ -9,21 +9,30 @@ BUILDINFO=$2
 OUTPUT_DIR=$3
 OUTPUT_FILENAME=$4
 
-BUILD_ID=$(jq -r '.build' "$BUILDINFO")
-
-echo "PROVENANCE FOR BUILD $BUILD_ID"
+BUILD_ID="$(jq -r '.build' "$BUILDINFO")"
+OUT="$OUTPUT_DIR/$OUTPUT_FILENAME"
 
 cd "$OUTPUT_DIR" || exit
 
-# generate buildtime sbom
-sbomnix "$IMAGE" --type=buildtime --depth=1
+# generate buildtime sbom (and suppress noisy stdout)
+sbomnix "$IMAGE" --type=buildtime --depth=1 > /dev/null 2>&1
 
 # generate the provenance file
 python3 /setup/provenance.py "$BUILDINFO" \
-    --out "$OUTPUT_DIR/$OUTPUT_FILENAME" \
+    --out "$OUT" \
     --sbom sbom.cdx.json \
     --ci-version "$(cat /setup/ci-version)"
 
 # clean up sbom files
 # they are no longer needed
 rm sbom*
+
+# add provenance to nix store
+PROVENANCE="$(nix-store --add "$OUT")"
+
+# sign the provenance file and upload both files to binary cache
+SIGNATURE="$(/setup/sign.sh "$PROVENANCE")"
+/setup/upload.sh "$PROVENANCE" "$SIGNATURE" > /dev/null 2>&1
+
+echo "PROVENANCE_LINK=\"$PROVENANCE\""
+echo "PROVENANCE_SIGNATURE=\"$SIGNATURE\""
