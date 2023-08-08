3d05a164bbd1c75e292ab0f8b320126bbd2e04af
Tero Tervala <tero.tervala@unikie.com>
2023-08-08 22:56:34 +0300

    Fix directory signing
    Directories should be now properly signed.
    These changes need to ne synced with scs-pki-research repo.
    
    Signed-off-by: Tero Tervala <tero.tervala@unikie.com>

--- Jenkinsfiles/signature-verification.sh
+++ Jenkinsfiles/signature-verification.sh
@@ -8,34 +8,55 @@
 # working directory[1], and is on a branch where signer container can be found,
 # and that yubikey has been provisioned on the machine
 #
+# sha256tree.py should be also in current directory
+#
 # [1] ~/Jenkins-agent/workspace/{env}/Supply_chain_security/signature_verification/
 # -------------------------------------------------------------------------
 
-# file and signature given should be nix store paths that are either
+# Calculate sha256 for a file or a directory
+function Calc_sha256sum {
+    python3 sha256tree.py --plain -- "$1"
+}
+
+if [ "$1" == "--check-script" ]; then
+    # (Note that official nix path never be --check-script)
+    # This just to check for errors on development environment that has shellcheck and bashate installed
+    if shellcheck --help > /dev/null 2>&1 && bashate --help > /dev/null 2>&1; then
+        if shellcheck "$0" && bashate -i E006 "$0"; then
+            echo "Nothing to complain"
+        fi
+    else
+        echo "Please install shellcheck and bashate to use check-script functionality"
+    fi
+    exit 1
+fi
+
+[ -z "$path_to_check" ] && { echo "Path not given" >&2; exit 1; }
+[ -z "$signature_file" ] && { echo "Signature file not given" >&2; exit 1; }
+
+# path_to_check and signature_file given should be nix store paths that are either
 # found locally or can be downloaded from cache.vedenemo.dev
 echo "Arguments:"
-echo "  file: '$file'"
-echo "  signature: '$signature'"
+echo "  path_to_check: '$path_to_check'"
+echo "  signature_file: '$signature_file'"
+
+# Stop on any error
+set -e
 
 set -x # debug
 
 # use nix profile
 export PATH=$PATH:/nix/var/nix/profiles/default/bin/
 
-# copy the file and it's signature from the binary cache if they don't exist locally
+# copy the file and it's signature_file from the binary cache if they don't exist locally
 COPY="nix copy --from https://cache.vedenemo.dev"
-[[ ! -f "$file" ]] && $COPY "$file"
-[[ ! -f "$signature" ]] && $COPY "$signature"
+[[ ! -e "$path_to_check" ]] && $COPY "$path_to_check"
+[[ ! -f "$signature_file" ]] && $COPY "$signature_file"
 
 # get the sha256 of the file being verified
-FILE_HASH="$(sha256sum "$file" | awk '{print $1}')"
+PATH_HASH="$(Calc_sha256sum "$path_to_check")"
 
-# get the signature, which for some reason includes dos line breaks
-SIGNATURE_CONTENT="$(cat "$signature" | dos2unix)"
-
-# verify the file using given signature
-# grep -q exits with code 1 if it doesn't find a match, failing the job
-scs-pki-research/yubikey/start.sh --verify \
-	--h=$FILE_HASH \
-    --sg=$SIGNATURE_CONTENT \
-    | grep -q "Signature is valid"
+# verify the file using given signature_file
+# start.sh verify returns zero if signature_file is valid.
+# Error code 10 for invalid signature_file, other codes signal that something else went wrong
+scs-pki-research/yubikey/start.sh verify "-h=$PATH_HASH" "-s=$(<"$signature_file")"
