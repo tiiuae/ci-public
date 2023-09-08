03495f722741e5712295f274cb8c9ad8305f76ea
Mika Nokka <mika.nokka@unikie.com>
2023-09-08 17:11:42 +0300

    Introduce Github PullRequest Hydra builder
    Detection of new repo PRs and changed PRs and building them using Hydra
    
    Signed-off-by: Mika Nokka <mika.nokka@unikie.com>

---
+++ pullrequests/setenv.sh
@@ -0,0 +1,10 @@
+export HYDRACTL_USERNAME="hydra"
+#HYDRACTL_PASSWORD for HYDRACTL_USERNAME needs to be setup also, do not use this file in order not to accidentally saving to public repo
+export TOKENFILE="./tokenfile"
+export TESTREPO="tiiuae/ghaf"
+export ORGANIZATION="tiiuae"
+export BUILDPRSFILE="pr2_data"
+export BUILDCHANGEDPRSFILE="pr2_changed_data"
+export HYDRACTL="../hydractl/hydractl.py" 
+export EXT_PORT="3032"
+export RUNDELAY="15" 
