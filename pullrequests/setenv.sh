bac7a4e820bc7a48d3b7ffb53e5925d70c6d3c34
Marko Lindqvist <marko.lindqvist@unikie.com>
2023-10-27 15:00:12 +0300

    PRBuilder setenv.sh: Set hydractl password
    Signed-off-by: Marko Lindqvist <marko.lindqvist@unikie.com>

--- pullrequests/setenv.sh
+++ pullrequests/setenv.sh
@@ -6,8 +6,7 @@ cd ../containers && . confs/hydra.default
 cd "${PRDIR}"
 
 export HYDRACTL_USERNAME="automation"
-#HYDRACTL_PASSWORD for HYDRACTL_USERNAME needs to be setup also, do not use this file in order not to accidentally saving to public repo
-# TODO: Get the password automatically from hydra configuration.
+export HYDRACTL_PASSWORD="${PW_AUTO}"
 export TOKENFILE="./tokenfile"
 export TESTREPO="tiiuae/ghaf"
 export ORGANIZATION="tiiuae"
