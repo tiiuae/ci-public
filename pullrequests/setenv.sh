a437fd7d4f4bbcaf0d8fdbcf00f08c4a70a25259
Marko Lindqvist <marko.lindqvist@unikie.com>
2023-10-27 14:57:41 +0300

    PRBuilder setenv.sh: Set EXT_PORT automatically
    Signed-off-by: Marko Lindqvist <marko.lindqvist@unikie.com>

--- pullrequests/setenv.sh
+++ pullrequests/setenv.sh
@@ -1,3 +1,10 @@
+
+PRDIR="$(pwd)"
+
+# Correct relative path for hydra.default to function correctly
+cd ../containers && . confs/hydra.default
+cd "${PRDIR}"
+
 export HYDRACTL_USERNAME="automation"
 #HYDRACTL_PASSWORD for HYDRACTL_USERNAME needs to be setup also, do not use this file in order not to accidentally saving to public repo
 # TODO: Get the password automatically from hydra configuration.
@@ -7,5 +14,5 @@ export ORGANIZATION="tiiuae"
 export BUILDPRSFILE="pr2_data"
 export BUILDCHANGEDPRSFILE="pr2_changed_data"
 export HYDRACTL="../hydractl/hydractl.py" 
-export EXT_PORT="3032"
-export RUNDELAY="15" 
+export EXT_PORT="${HC_PORT}"
+export RUNDELAY="15"
