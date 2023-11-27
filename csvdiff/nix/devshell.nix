e0aa3b125b33678f1b2ac7e566dc30b931752bea
Henri Rosten <henri.rosten@unikie.com>
2023-11-27 09:19:07 +0200

    Remove python venv usage
    Signed-off-by: Henri Rosten <henri.rosten@unikie.com>

--- csvdiff/nix/devshell.nix
+++ csvdiff/nix/devshell.nix
@@ -7,29 +7,25 @@
       name = "csvdiff-dev-shell";
 
       packages = with pkgs; [
-        python3.pkgs.black
-        python3.pkgs.colorlog
-        python3.pkgs.deploykit
-        python3.pkgs.invoke
-        python3.pkgs.numpy
-        python3.pkgs.pandas
-        python3.pkgs.pycodestyle
-        python3.pkgs.pylint
-        python3.pkgs.pytest
-        python3.pkgs.tabulate
-        python3.pkgs.venvShellHook
         reuse
+        (
+          pkgs.python3.withPackages (ps:
+            with ps; [
+              black
+              colorlog
+              deploykit
+              invoke
+              numpy
+              pandas
+              pycodestyle
+              pylint
+              pytest
+              tabulate
+            ])
+        )
       ];
-
-      venvDir = "venv";
-      postShellHook = ''
+      shellHook = ''
         export PYTHONPATH="$PYTHONPATH:$(pwd)"
-
-        # https://github.com/NixOS/nix/issues/1009:
-        export TMPDIR="/tmp"
-
-        # Enter python development environment
-        make install-dev
       '';
     };
   };
