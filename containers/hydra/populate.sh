f27b705a18a732f8c817be39df933171daa53035
Marko Lindqvist <marko.lindqvist@unikie.com>
2023-12-07 10:46:36 +0200

    hydra: Keep preinstalled packages safe from GC
    Signed-off-by: Marko Lindqvist <marko.lindqvist@unikie.com>

--- containers/hydra/populate.sh
+++ containers/hydra/populate.sh
@@ -8,10 +8,12 @@
 # This works around a problem that hydra cannot "build" (fetch)
 # some initial source packages before it has been bootstrapped
 # Here we pre-populate the store with those required packages.
+mkdir -p /home/hydra/packages/nix/store
 cat /setup/packages.lst |
     ( while read PACKAGE DISCARD_REST
       do
 	if [ "$PACKAGE" != "" ] && [ "$PACKAGE" != "#" ] ; then
           nix-env -i "$PACKAGE"
+          nix-store --add-root /home/hydra/packages${PACKAGE} --indirect --realise "$PACKAGE"
 	fi
       done )
