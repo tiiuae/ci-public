8829816126642bc4c6dc5520ffbd7f50ee007612
Marko Lindqvist <marko.lindqvist@unikie.com>
2024-03-18 10:27:55 +0200

    hydra: Update sbomnix
    To commit 9df4fb380b94fce54c4374958e7fc69a573cf6ec
    
    Signed-off-by: Marko Lindqvist <marko.lindqvist@unikie.com>

--- containers/hydra/install-sbomnix.sh
+++ containers/hydra/install-sbomnix.sh
@@ -3,7 +3,7 @@
 # SPDX-FileCopyrightText: 2023 Technology Innovation Institute (TII)
 # SPDX-License-Identifier: Apache-2.0
 
-COMMIT_HASH=055bf464f1a3344870a40f337caa492a69f69dbb
+COMMIT_HASH=9df4fb380b94fce54c4374958e7fc69a573cf6ec
 
 git clone https://github.com/tiiuae/sbomnix
 cd sbomnix || exit
